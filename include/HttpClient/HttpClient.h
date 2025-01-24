2da064b76477b1777e21142b57f9b48ded7ea20a
prayushU <prayushudas@gmail.com>
2025-01-24 00:07:17 +0000

    fixed issue for unwanted object delection when async

--- include/HttpClient/HttpClient.h
+++ include/HttpClient/HttpClient.h
@@ -24,29 +24,31 @@ struct HttpClient : std::enable_shared_from_this<HttpClient> {
 
     _resolver.async_resolve(
         connectionParameter.host, connectionParameter.port,
-        [f = std::move(callable), sharedThis = this->shared_from_this(), this](
-            const boost::system::error_code &err,
-            const boost::asio::ip::tcp::resolver::results_type
-                &endpoints) mutable {
+        [f = std::move(callable), sharedThis = this->shared_from_this(),
+         this](const boost::system::error_code &err,
+               const boost::asio::ip::tcp::resolver::results_type
+                   &endpoints) mutable {
           if (err) {
             std::cout << "err occurred: " << err.message() << std::endl;
             f(err);
+            DeferDelection(); // keeping the shared pointer alive
             return;
           }
 
           boost::asio::async_connect(
               _socket, endpoints,
-              [f2 = std::move(f), sharedThis = std::move(sharedThis)](
-                  boost::system::error_code err,
-                  boost::asio::ip::tcp::endpoint) {
+              [f2 = std::move(f), sharedThis = std::move(sharedThis),
+               this](boost::system::error_code err,
+                     boost::asio::ip::tcp::endpoint) {
                 if (err) {
-                  std::cout
-                      << "Cannot connect error occurred : " << err.message()
-                      << std::endl;
+                  std::cout << "Connect error occurred : " << err.message()
+                            << std::endl;
                   f2(err);
+                  DeferDelection();
                   return;
                 }
                 f2(std::error_code());
+                DeferDelection();
               });
           // std::cout << "successfully resolved " << std::endl;
           // f(std::error_code());
@@ -61,6 +63,7 @@ struct HttpClient : std::enable_shared_from_this<HttpClient> {
                         HttpResponse response;
                         response.Body = "glad to know you";
                         f(std::error_code(), std::move(response));
+                        DeferDelection();
                       });
   }
 
@@ -73,6 +76,13 @@ private:
   HttpClientParameters _httpClientParameters;
   boost::asio::ip::tcp::resolver _resolver;
   boost::asio::ip::tcp::socket _socket;
+
+  void DeferDelection() {
+    boost::asio::post(_httpClientParameters._executor,
+                      [sharedThis = this->shared_from_this()]() mutable {
+                        sharedThis.reset();
+                      });
+  }
 };
 
 #endif
